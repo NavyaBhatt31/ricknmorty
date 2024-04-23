ff0122fe7fb738f76edf05136418d17338e5c710
Navya <navya.bhatt31@gmail.com>
2024-04-23 16:40:20 +0530

    layout changed, link and routing added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,44 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
+import Pagination from './components/Pagination/Pagination';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CardDetails from './components/Cards/CardDetails';
+
 
 function App() {
-  let [pageNumber, setPageNumber] = useState(1);
+  return (
+    <Router>
+      <div className="App">
+
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/:id" element={<CardDetails />} /> 
+      </Routes>
+    </Router>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+const Home=() => {
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
   let [fetchedData, updateFetchedData] = useState([]);
   let {info, results} = fetchedData;
-  
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&status=${status}&gender=${gender}&species=${species}`;
 
   useEffect(()=>{
 
@@ -21,23 +52,32 @@ function App() {
   },[api])
 
   return (
-    <div className="App">
-     <h1 className="text-center ubuntu my-5">Rick & Morty <span className="text-primary">App</span> </h1>      
+    <div className="App" style={{ backgroundColor: '#272b33', minHeight: '100vh', color: '#fff' }}>     
+    <h1 className="text-center Ubuntu "style={{ marginBottom: '30px' }}>Rick & Morty <span className="text-primary">App</span> </h1>      
      <div className="container">
-      <div className="row">
-        <div className="col-3">
-          <Filters/>
-        </div>
-        <div className="col-8">
+      <div className="row" >
+      <Filters
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />        <div className="col-8">
         <div className="row">
-          <Cards results={results} />
+          <Cards page="/" results={results} />
         
         </div>
         </div>
       </div>
      </div>
+     <Pagination 
+     info={info} 
+     pageNumber={pageNumber} 
+     updatePageNumber={updatePageNumber} 
+     />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import styles from "./Cards.module.scss";
 
-const Cards = ({ results }) => {
+const Cards = ({ results, page }) => {
   let display;
   console.log(results);
 
   if (results) {
     display = results.map(({ id, name, image, location, status }) => {
-      return(
-      <div key={id} className={`col-6 position-relative`} style={{ marginBottom: '20px'}}>
+     
+       return(
+      <Link 
+      to={`${page}${id}`}
+      key={id} className={`col-6 position-relative`} style={{ marginBottom: '20px'}}>
         <div className={`${styles.cards} d-flex flex-column`}>
         <div className="d-flex"> {/* Container for image and content */}
           <img
@@ -17,7 +22,7 @@ const Cards = ({ results }) => {
             className={`${styles.img} img-fluid`}
             style={{ width: '180px', height: '180px', marginRight: '20px' }} // Adjust width and height as needed
           /> {/* Image */}
-          <div className="content"> {/* Content */}
+            <div className={`${styles.content} text-dark`}>
             <div className="fs-4 fw-bold mb-2">{name}</div>
             <div className="d-flex align-items-center"> {/* Container for status and location */}
             {(()=>{
@@ -51,7 +56,7 @@ const Cards = ({ results }) => {
         </div>
       </div>
 
-    </div>
+    </Link>
     );
    });
   } else {
